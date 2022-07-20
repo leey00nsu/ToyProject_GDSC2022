dd159cae325cf14a69ec87c923b7786a2a066c69
GoraniPie <scn09050@gmail.com>
2022-07-20 23:19:37 +0900

    이미지 업로드 기능 추가

--- contents/models.py
+++ contents/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class NewPost(models.Model):
+    user = models.TextField(max_length=40, null=True)
     date = models.DateTimeField(auto_now_add=True)
     title = models.TextField(max_length=40, null=True)
     imgfile = models.ImageField(null=True, upload_to="", blank=True)
--- contents/views.py
+++ contents/views.py
@@ -9,10 +9,11 @@ def post_new(request):
     if request.method == 'POST':
         
         post = NewPost()
-        
+        post.user = request.user.username
         post.date = datetime.datetime.now()
         post.title = request.POST['title']
         post.content = request.POST['body']
+        post.imgfile = request.FILES['imgfile']
         
         post.save()
        

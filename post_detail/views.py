dd6a59a216380cf8767037a89cdcb4a20fbd26df
leeyoonsu <dbstndla1212@naver.com>
2022-07-29 14:40:40 +0900

    :twisted_rightwards_arrows: suminhand 브랜치와 머지
    .

--- post_detail/views.py
+++ post_detail/views.py
@@ -6,6 +6,6 @@ def open_post(request, id):
     OpenPost = get_object_or_404(NewPost, pk=id)
     
     #contents/content12312.html로 세부내용을 OpenPost라는 이름으로 보냄.
-    return render(request, 'contents/content12312.html', {'OpenPost' : OpenPost})
+    return render(request, 'contents/content_view.html', {'OpenPost' : OpenPost})
 
 

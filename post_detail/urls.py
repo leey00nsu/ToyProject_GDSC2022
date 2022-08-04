7939f5d80d24a3adb275636e12a3b94565d265ab
suminhand <104995607+suminhand@users.noreply.github.com>
2022-08-04 18:37:07 +0900

    포스트 목록/아직 수정 필요

--- post_detail/urls.py
+++ post_detail/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('<int:id>/',views.open_post, name='open_post'),
+    path('', views.post_list, name='post_list')
 ]
 #포스트 내용을 보기위한 링크는 https://기본적 주소/post_detail/id(1이나 2 ... 몇번째 게시문인지 표시)/
--- post_detail/views.py
+++ post_detail/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from contents.models import NewPost
 
+#세부내용 보는 창
 def open_post(request, id):
     #OpenPost로 db에서 세부내용을 가져옴.
     OpenPost = get_object_or_404(NewPost, pk=id)
@@ -8,4 +9,8 @@ def open_post(request, id):
     #contents/content12312.html로 세부내용을 OpenPost라는 이름으로 보냄.
     return render(request, 'contents/content_view.html', {'OpenPost' : OpenPost})
 
-
+#목록 보는 창
+def post_list(request):
+    PostList = NewPost.objects.filter(user = request.user.username)
+    PostList = PostList.order_by('-date')
+    return render(request, "contents/content.html", {'PostList' : PostList})
